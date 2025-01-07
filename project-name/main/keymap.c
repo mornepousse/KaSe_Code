911d190fa991e121df2031cbedcba65db1c3f625
Mae PUGIN <48982737+mornepousse@users.noreply.github.com>
2025-01-07 18:19:53 +0100

    ajustement du mapping des touches

--- project-name/main/keymap.c
+++ project-name/main/keymap.c
@@ -34,9 +34,9 @@ uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    {
 	{KC_ESC,  KC_ENT,/**/	KC_2,    	KC_3,    KC_4,    	KC_5,    KC_6,    	KC_7,    	KC_8,    KC_9,/**/  KC_0,		KC_MINUS,    /**/KC_MINUS },
 	{KC_DEL,  KC_1,    		KC_W,    	KC_E,    KC_R,    	KC_T,    KC_Y,    	KC_U,    	KC_I,    KC_O,    	KC_0,    	KC_RBRC,    /**/KC_INT1},
-	{KC_TAB,  KC_Q,    		KC_S,    	KC_D,    KC_F,    	KC_G,    KC_INT1, 	KC_J,    	KC_K,    KC_L,    	KC_P, 		KC_LBRC,     /**/KC_EQUAL},
+	{KC_TAB,  KC_Q,    		KC_S,    	KC_D,    KC_F,    	KC_G,    KC_H, 	KC_J,    	KC_K,    KC_L,    	KC_P, 		KC_LBRC,     /**/KC_EQUAL},
 	{KC_RALT, KC_A,    		KC_X,    	KC_C,    KC_V,    	KC_B,    KC_N,    	KC_M,    	KC_COMM, KC_DOT,  	KC_SCLN, 	KC_QUOT,     /**/KC_DELETE},
-	{KC_LCTRL,KC_Z, 		KC_LALT, 	KC_LWIN, KC_LSHIFT, KC_SPC,  KC_BSPACE,	KC_ENTER,   KC_NUHS, KC_ENTER,	KC_SLSH,  	KC_GRV,      /**/KC_NO }
+	{KC_LCTRL,KC_Z, 		KC_LALT, 	KC_LWIN, KC_LSHIFT, KC_SPACE,  KC_BSPACE,	KC_ENTER,   KC_NUHS, KC_DELETE,	KC_SLSH,  	KC_GRV,      /**/KC_NO }
 	},
    {
 	{KC_NO,  KC_NO,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_NO,           KC_NO, KC_NO},
